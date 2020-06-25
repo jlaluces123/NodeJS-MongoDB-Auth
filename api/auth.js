f046b2687a0ee63d9c78ee22d733ac2fb91ae295
unknown <jlalucescareer123@gmail.com>
2020-06-25 06:56:35 -0700

    Deleted Google Setup: no need to use after discussing with client

--- api/auth.js
+++ api/auth.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const { response } = require('express');
+const passport = require('passport');
+
 const router = express.Router();
 
 // Login Function
 router.get('/login', (req, res) => {
     res.send('Login.');
-});
-
-// Login w/ Google
-router.get('/google', (req, res) => {
-    res.send('Logging in with Google.');
 });
 
 // Logout Function
