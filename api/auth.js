5a39f91a0724f965e5f1b785b9ab233e003eb225
unknown <jlalucescareer123@gmail.com>
2020-06-25 07:10:59 -0700

    Notes + Setup: added setup files as well as notes on the function steps

--- api/auth.js
+++ api/auth.js
@@ -3,13 +3,31 @@ const passport = require('passport');
 
 const router = express.Router();
 
-// Login Function
-router.get('/login', (req, res) => {
+/*
+Signup Function:
+    [ @method: POST - @param: '/signup' ]
+    1. Validate Email + Password
+    2. Find if User already exists
+    3. Create a User with information + save User in DB
+*/
+router.post('/signup', (req, res) => {
+    res.send('Signing Up.');
+});
+
+/*
+Login Function:
+    [ @method: POST - @param: '/login' ]
+    1. Validate email + password
+    2. Find if user exists or not
+    3. Compare password
+    4. Return message if wanted
+*/
+router.post('/login', (req, res) => {
     res.send('Login.');
 });
 
 // Logout Function
-router.get('/logout', (req, res) => {
+router.post('/logout', (req, res) => {
     res.send('Logging Out.');
 });
 
---
+++ helpers/validators.js
@@ -0,0 +1,1 @@
+// File is for express-validator code chunks that are repeated many times
