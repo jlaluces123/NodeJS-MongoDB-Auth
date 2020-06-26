cb6ccd280447a8c1b4c0834eb671200a07030946
unknown <jlalucescareer123@gmail.com>
2020-06-26 09:21:29 -0700

    Connect to MongoDB: completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 // File Imports
@@ -12,6 +13,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // * Connect to MongoDB
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0-pjusq.mongodb.net/<${process.env.MONGO_DB_NAME}>?retryWrites=true&w=majority`
+);
 
 // * Routes:
 app.use('/api/auth', authRouter);
